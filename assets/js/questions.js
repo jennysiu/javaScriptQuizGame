eac869d6a6b7c329a964b9d20ea4e2c22f22495e
jennysiu <jenny.siu79@gmail.com>
2023-12-22 20:30:13 +0100

    timer function added

---
+++ assets/js/questions.js
@@ -0,0 +1,31 @@
+// QUESTIONS ON JAVASCRIPT
+
+[
+  {
+    "question": "Who invented JavaScript?",
+    "choices": ["Sophia Martinez", "Brendan Eich", "Emma Resig", "Douglas Crockford"],
+    "correctAnswer": "Brendan Eich"
+  },
+  {
+    "question": "Which of the following is a primitive data type in JavaScript?",
+    "choices": ["Object", "String", "Function", "Array"],
+    "correctAnswer": "String"
+  },
+  // {
+  //   "question": "Which of the following is a primitive data type in JavaScript?",
+  //   "choices": ["Object", "String", "Function", "Array"],
+  //   "correctAnswer": "String"
+  // },
+  // {
+  //   "question": "Which of the following is a primitive data type in JavaScript?",
+  //   "choices": ["Object", "String", "Function", "Array"],
+  //   "correctAnswer": "String"
+  // },
+  // {
+  //   "question": "Which of the following is a primitive data type in JavaScript?",
+  //   "choices": ["Object", "String", "Function", "Array"],
+  //   "correctAnswer": "String"
+  // }
+  
+  // Add more questions here
+]
