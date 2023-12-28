5e17b9c0fd10d01d38607a7f4952665e17dfd18c
jennysiu <jenny.siu79@gmail.com>
2023-12-28 01:28:38 +0100

    shows next question after user answers

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -10,22 +10,22 @@ const quizQuestions = [
     "question": "Which of the following is a primitive data type in JavaScript?",
     "choices": ["Object", "String", "Function", "Array"],
     "correctAnswer": "String"
+  },
+  {
+    "question": "When an operator’s value is NULL, the typeof returned by the unary operator is:",
+    "choices": ["Boolean", "Undefined", "Object", "Integer"],
+    "correctAnswer": "Object"
+  },
+  {
+    "question": "How to stop an interval timer in Javascript?",
+    "choices": ["clearInterval", "clearTimer", "intervalOver", "None of the above"],
+    "correctAnswer": "clearInterval"
+  },
+  {
+    "question": "Javascript is an _______ language?",
+    "choices": ["Object-Oriented", "Object-Based", "Procedural", "None of the above"],
+    "correctAnswer": "Object-Oriented"
   }
-  // {
-  //   "question": "Which of the following is a primitive data type in JavaScript?",
-  //   "choices": ["Object", "String", "Function", "Array"],
-  //   "correctAnswer": "String"
-  // },
-  // {
-  //   "question": "Which of the following is a primitive data type in JavaScript?",
-  //   "choices": ["Object", "String", "Function", "Array"],
-  //   "correctAnswer": "String"
-  // },
-  // {
-  //   "question": "Which of the following is a primitive data type in JavaScript?",
-  //   "choices": ["Object", "String", "Function", "Array"],
-  //   "correctAnswer": "String"
-  // }
   
 ]
 
