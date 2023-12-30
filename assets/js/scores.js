85e5d44610bee20a9edc3e03a7825dad97a61a5e
jennysiu <jenny.siu79@gmail.com>
2023-12-30 23:21:57 +0100

    function to clear highscores

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -14,22 +14,27 @@ renderHighscores();
 function renderHighscores() {
   // retrieve from local storage 
   highscores = JSON.parse(localStorage.getItem("localHighscores"));
+  // sort arrar descending 
+  highscores.sort((a, b) => b.score - a.score);
   console.log(highscores);
-  let highscoresWithSpaces = highscores.join(" ")
   
   for (let i = 0; i < highscores.length; i++) {
-
     let highscoreItem = document.createElement("li");
     highscoreItem.textContent = `${highscores[i].initials}, Score: ${highscores[i].score}`
     highscoresList.appendChild(highscoreItem)
   }
 }
 
-
 clearHighScoresButton.addEventListener("click", function() {
   highscores = [];
   // Update the local storage
-  localStorage.setItem('highscores', JSON.stringify(highscores));
+  localStorage.setItem('localHighscores', JSON.stringify(highscores));
 
+  // clear existing items appended on page
+  const highscoreItems = document.querySelectorAll("li");
+  for (const item of highscoreItems) {
+    item.remove(); // Remove all buttons
+    }
+  
 })
 
